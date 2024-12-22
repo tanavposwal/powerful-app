4eb5df1940a99d07e75c002eff22e8c9776d227a
tanavposwal <tanavposwal939@gmail.com>
2024-12-22 23:41:18 +0530

    db

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+  title = models.CharField(max_length=100)
+  is_done = models.BooleanField(default=False)
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def todos(request):
+  todos = TodoItem.objects.all()
+  return render(request, 'todos.html', {'todos': todos})
